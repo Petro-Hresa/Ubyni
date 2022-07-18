c9a6e3d4d68ac0e696cb8766f12a6df0ac5d518e
Petro-Hresa <gresapetro379@gmail.com>
2022-07-18 23:11:18 +0300

    Add component Home

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import RegistrationContainer from "./components/Registration/RegistrationContainer";
 import LoginContainer from "./components/Login/LoginContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import Home from "./components/Home/Home";
 import './../src/assets/styles/style.scss'
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
    <BrowserRouter>
       <Header/>
         <Routes>
+        <Route path="/" element={ <Home/> }/>
           <Route path="/registration" element={ <RegistrationContainer/> }/>
           <Route path="/login" element={ <LoginContainer/> }/>
           <Route path="/users" element={ <UsersContainer/> }/>
