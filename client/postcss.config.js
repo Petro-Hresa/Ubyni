550668fed5601709ca0548bb1df43e918c8f93b0
Petro-Hresa <gresapetro379@gmail.com>
2022-08-05 10:23:50 +0300

    add tailwindcss to project

---
+++ client/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports =  {
+    plugins: [
+
+        'tailwindcss',
+        'autoprefixer'
+        // require('tailwindcss'),
+        // require('autoprefixer'),
+        // require('css-mqpacker'),
+        // (process.env.NODE_ENV === 'production' ?  require('cssnano')  :  null),
+    ]
+}
