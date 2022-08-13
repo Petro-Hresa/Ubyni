5e901cf79c3bcf344eea87214f8c308e1c0f17a3
Petro-Hresa <gresapetro379@gmail.com>
2022-08-13 20:26:56 +0300

    Add styled-components to the projec. And united with tailwind css add styles to class Wrap

--- client/pages/components/Navbar/_Navbar.tsx
+++ client/pages/components/Navbar/_Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Burger from '../Burger/_Burger';
 import { Nav } from '../Nav/_Nav';
 import Lang from '../Lang/_Lang'
@@ -7,9 +7,38 @@ import { Button } from '../Button/_Button';
 import Besides from '../Besides/_Besides';
 import { useSelector } from 'react-redux';
 import { TRootState } from '../../store/store';
+import styled from 'styled-components';
+import tw from "tailwind-styled-components";
+
+
 
 const Navbar = () => {
   const open = useSelector((state:TRootState)=> state.besides.isOpen)
+  const wrapRef = useRef<any>(null)
+  const boxRef = useRef<any>(null)
+
+
+
+  useEffect(()=>{
+    // let  styles = {
+    //   height: besidesBox.current.offsetHeight
+    //  }
+    // heightOpen = ()=> {
+    //   if(open){
+    //     return besidesBox.current.offsetHeight
+    //   }
+    // }
+  //  besidesBox.current.offsetHeight;
+    // open ? besidesBox.current.style =  {height:besidesBox.current.offsetHeight} : ''
+    // besidesBox.current
+  },[])
+
+  const heandleClick = ()=> {
+    const wrapper = boxRef.current;
+    wrapper.classList.toggle('is-nav-open')
+  }
+  
+  // ${open ? 'max-md:h-auto' : 'max-md:h-0'}
 
   return (
     <div className="block-fixed w-full z-20 left-0 top-0 bg-primary flex justify-between items-center relative py-30 max-md:py-15">
@@ -18,25 +47,55 @@ const Navbar = () => {
           <Nav/>
       </div>
 
-      <div className="flex space-x-30 max-md:relative items-center">
+      <div className="flex space-x-30 items-center">
         
-        {open &&
-          <div className="flex max-md: max-md:flex-col-reverse md:space-x-30 max-md:absolute max-md:top-[calc(100%)] bg-primary -right-30 max-md:p-30 rounded-bl-md max-md:-z-[5]"> 
-            <Lang/>
-            <Link href={'/registration'}>
-                <a className={"inline-block text-sm-xx md:text-md leading-12 text-shadow text-secondary max-md:mb-20"}>Registration</a>
-            </Link> 
-          </div> 
-       
-        }
-        <Link href={'/login'}>
-          <a  className="bg-secondary py-2 px-20 rounded-[5px] font-semibold text-primary text-sm-xx md:text-md leading-12 flex-shrink-0 flex-grow-0">Log in</a>
-        </Link>
-
+     
+      <div className="max-md:relative">
         <Besides/>
+        <Wrap ref={wrapRef} open={open}> 
+            <div ref={boxRef}  className="flex max-md:flex-col-reverse md:space-x-30 max-md:py-30">
+              <Lang/>
+              <Link href={'/registration'}>
+                  <a className={"inline-block text-sm-xx md:text-md leading-12 text-shadow text-secondary max-md:px-30 max-md:mb-20"}>Registration</a>
+              </Link> 
+            </div>
+        </Wrap>
+      </div>  
+        
+
+      <Link href={'/login'}>
+        <a  className="bg-secondary py-2 px-20 rounded-[5px] font-semibold text-primary text-sm-xx md:text-md leading-12 flex-shrink-0 flex-grow-0">Log in</a>
+      </Link>
+
+     
       </div>
     </div>
   )
-}
+};
+
+
+
 
 export default Navbar;
+
+
+
+type TOpen = {
+  open: boolean
+}
+
+const Wrap = tw.div`
+  ${(box:TOpen)=> (box.open ? "h-[120px]" : "h-0")}
+  overflow-hidden 
+  bg-primary 
+  max-md:absolute 
+  top-[calc(100%)] 
+  max-md:-left-[31px]  
+  max-md:rounded-bl-md 
+  max-md:-z-[5] 
+  max-md: 
+  min-w-[190px] 
+  transition-all
+`;
+
+
